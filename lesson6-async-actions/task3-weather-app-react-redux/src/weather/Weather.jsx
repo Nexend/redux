2776a930e064e418f95fefe56cab1ce03fd1c23a
GITHUB Nexend <GITHUB ddante@ukr.net>
2021-02-15 17:34:40 +0300

    task3

---
+++ lesson6-async-actions/task3-weather-app-react-redux/src/weather/Weather.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { weatherDataSelector } from './weather.selectors';
+import * as weathersActions from './weather.actions';
+
+const Weather = ({ weatherData, fetchWeatherData }) => {
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
+  return (
+    <main className="weather">
+      <h1 className="weather__title">Weather data</h1>
+      <ul className="cities-list">
+        {weatherData.map(data => (
+          <li key={data.id} className="city">
+            <span className="city__name">{data.name}</span>
+            <span className="city__temperature">{`${data.temperature} F`}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+const mapState = state => {
+  return {
+    weatherData: weatherDataSelector(state),
+  };
+};
+
+const mapDispatch = {
+  fetchWeatherData: weathersActions.fetchWeatherData,
+};
+
+export default connect(mapState, mapDispatch)(Weather);
